d5ea8204ac9a7581dffb969fc7e92db4a8f09866
ruwpy <ruwpy.who@gmail.com>
2024-07-07 17:23:08 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    env, fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
@@ -11,7 +12,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => handle_connection(stream),
+            Ok(stream) => {
+                std::thread::spawn(|| handle_connection(stream));
+            }
             Err(e) => {
                 println!("error: {}", e);
             }
@@ -25,48 +28,61 @@ fn handle_connection(mut stream: TcpStream) {
 
     let request = String::from_utf8(buffer.into()).unwrap();
 
-    println!("{}", request);
-
-    let mut start_line = String::new();
     let mut headers: HashMap<String, String> = HashMap::new();
 
-    for (index, line) in request.lines().enumerate() {
-        if index == 0 {
-            start_line = line.to_string();
-        };
+    let (start_line, request_details) = request.split_once("\r\n").unwrap();
+    let (headers_data, _) = request_details.split("\r\n\r\n").next_tuple().unwrap();
 
-        if index >= 1 {
-            if line.is_empty() {
-                break;
-            }
+    for line in headers_data.lines() {
+        let (key, value) = line.split(": ").next_tuple().unwrap();
 
-            println!("{}\r\n", line);
-
-            let (key, value) = line.split(": ").next_tuple().unwrap();
-
-            headers.insert(key.to_string(), value.to_string());
-        }
+        headers.insert(key.to_string(), value.to_string());
     }
 
-    let (method, uri, http_version) = start_line.split(" ").next_tuple().unwrap();
+    let (method, uri, _) = start_line.split(" ").next_tuple().unwrap();
 
     if method.eq("GET") {
-        let (status_line, contents) = if uri.starts_with("/echo") {
-            let message = uri.split("/").nth(2).unwrap();
+        let (status_line, contents, content_type, content_length) = match uri {
+            user_agent if user_agent == "/user-agent" => {
+                let user_agent_header = headers.get("User-Agent").unwrap().as_str();
 
-            ("HTTP/1.1 200 OK\r\n", message)
-        } else if uri.eq("/") {
-            ("HTTP/1.1 200 OK\r\n", "Hello, World!")
-        } else if uri.eq("/user-agent") {
-            let user_agent_header = headers.get("User-Agent").unwrap().as_str();
+                (
+                    "HTTP/1.1 200 OK\r\n",
+                    user_agent_header,
+                    "text/plain",
+                    user_agent_header.len(),
+                )
+            }
+            echo if echo.starts_with("/echo/") => {
+                let message = uri.split("/").nth(2).unwrap();
 
-            ("HTTP/1.1 200 OK\r\n", user_agent_header)
-        } else {
-            ("HTTP/1.1 404 Not Found\r\n", "Not Found")
+                ("HTTP/1.1 200 OK\r\n", message, "text/plain", message.len())
+            }
+            file if file.starts_with("/files/") => {
+                let filename = uri.split("/").nth(2).unwrap();
+
+                let env_args: Vec<String> = env::args().collect();
+                let mut dir = env_args[2].clone();
+                dir.push_str(filename);
+
+                let file = fs::read(dir);
+
+                match file {
+                    Ok(f) => (
+                        "HTTP/1.1 200 OK\r\n",
+                        "",
+                        "application/octet-stream",
+                        f.len(),
+                    ),
+                    Err(..) => ("HTTP/1.1 404 Not Found\r\n", "Not Found", "text/plain", 9),
+                }
+            }
+            index if index == ("/") => ("HTTP/1.1 200 OK\r\n", "Hello, World!", "text/plain", 13),
+            _ => ("HTTP/1.1 404 Not Found\r\n", "Not Found", "text/plain", 9),
         };
 
-        let content_type_header = "Content-Type: text/plain";
-        let content_length_header = format!("Content-Length: {}", contents.len());
+        let content_type_header = format!("Content-Type: {}", content_type);
+        let content_length_header = format!("Content-Length: {}", content_length);
 
         let response = format!(
             "{}{}\r\n{}",
