907618ef14d4476d623f5b7b040ffa1851160362
ruwpy <ruwpy.who@gmail.com>
2024-07-11 14:25:07 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,43 @@ use std::{
 
 use itertools::Itertools;
 
+enum ContentType {
+    PlainText,
+    OctetStream,
+}
+
+struct Response {
+    status_line: String,
+    data: String,
+    content_type: String,
+    content_length: u16,
+}
+
+impl Response {
+    fn format_to_string(self) -> String {
+        let headers = HashMap::from([
+            ("Content-Type", self.content_type),
+            ("Content-Length", self.content_length.to_string()),
+        ]);
+
+        let headers_str = headers
+            .into_iter()
+            .map(|header| {
+                let (k, v) = header;
+
+                return format!("{}: {}", k, v);
+            })
+            .join("\r\n");
+
+        let formatted_response = format!(
+            "{}\r\n{}\r\n\r\n{}",
+            self.status_line, headers_str, self.data
+        );
+
+        formatted_response
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
@@ -42,21 +79,16 @@ fn handle_connection(mut stream: TcpStream) {
     let (method, uri, _) = start_line.split(" ").next_tuple().unwrap();
 
     if method.eq("GET") {
-        let (status_line, contents, content_type, content_length) = match uri {
+        let response: Response = match uri {
             user_agent if user_agent == "/user-agent" => {
                 let user_agent_header = headers.get("User-Agent").unwrap().as_str();
 
-                (
-                    "HTTP/1.1 200 OK\r\n",
-                    user_agent_header,
-                    "text/plain",
-                    user_agent_header.len(),
-                )
+                create_response(200, user_agent_header.to_string(), ContentType::PlainText)
             }
             echo if echo.starts_with("/echo/") => {
                 let message = uri.split("/").nth(2).unwrap();
 
-                ("HTTP/1.1 200 OK\r\n", message, "text/plain", message.len())
+                create_response(200, message.to_string(), ContentType::PlainText)
             }
             file if file.starts_with("/files/") => {
                 let filename = uri.split("/").nth(2).unwrap();
@@ -65,35 +97,42 @@ fn handle_connection(mut stream: TcpStream) {
                 let mut dir = env_args[2].clone();
                 dir.push_str(filename);
 
-                let file = fs::read(dir);
+                let file = fs::read_to_string(dir);
 
                 match file {
-                    Ok(f) => (
-                        "HTTP/1.1 200 OK\r\n",
-                        "",
-                        "application/octet-stream",
-                        f.len(),
-                    ),
-                    Err(..) => ("HTTP/1.1 404 Not Found\r\n", "Not Found", "text/plain", 9),
+                    Ok(f) => create_response(200, f, ContentType::OctetStream),
+                    Err(_) => create_response(404, "Not Found".to_string(), ContentType::PlainText),
                 }
             }
-            index if index == ("/") => ("HTTP/1.1 200 OK\r\n", "Hello, World!", "text/plain", 13),
-            _ => ("HTTP/1.1 404 Not Found\r\n", "Not Found", "text/plain", 9),
+            index if index == ("/") => {
+                create_response(200, "Hello, World!".to_string(), ContentType::PlainText)
+            }
+            _ => create_response(404, "Not Found".to_string(), ContentType::PlainText),
         };
 
-        let content_type_header = format!("Content-Type: {}", content_type);
-        let content_length_header = format!("Content-Length: {}", content_length);
-
-        let response = format!(
-            "{}{}\r\n{}",
-            status_line,
-            format!("{}\r\n{}\r\n", content_type_header, content_length_header),
-            contents
-        );
-
-        println!("{}", response);
-
-        stream.write(response.as_bytes()).unwrap();
+        stream
+            .write(response.format_to_string().as_bytes())
+            .unwrap();
         stream.flush().unwrap();
+    }
+}
+
+fn create_response(status_code: u16, data: String, content_type: ContentType) -> Response {
+    let status_line = match status_code {
+        200 => "HTTP/1.1 200 OK",
+        404 => "HTTP/1.1 404 Not Found",
+        _ => "HTTP/1.1 400 Bad Request",
+    };
+
+    let content_type_str = match content_type {
+        ContentType::OctetStream => "application/octet-stream",
+        ContentType::PlainText => "text/plain",
+    };
+
+    Response {
+        status_line: status_line.to_string(),
+        content_length: data.len() as u16,
+        content_type: content_type_str.to_string(),
+        data,
     }
 }
